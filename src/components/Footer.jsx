a6c7e2d59956cea6d047cf090a5b003ee7349b37
Theeskyes <galaksiputra2009@gmail.com>
2026-06-08 13:13:25 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,9 +21,9 @@ const Footer = () => {
 
         {/* Ikon Sosmed - di tengah di mobile */}
         <div className="flex gap-3 order-2 md:order-none">
-          <a href="https://github.com/rissss21"><i className="ri-github-fill ri-2x"></i></a>
-          <a href="https://www.instagram.com/farisedrikprayoga/"><i className="ri-instagram-fill ri-2x"></i></a>
-          <a href="https://www.youtube.com/@FarisEdrikPrayoga"><i className="ri-youtube-fill ri-2x"></i></a>
+          <a href="github.com/lias1726"><i className="ri-github-fill ri-2x"></i></a>
+          <a href="https://www.instagram.com/aawa.4u/"><i className="ri-instagram-fill ri-2x"></i></a>
+          <a href="https://youtube.com/@awaa.4uu"><i className="ri-youtube-fill ri-2x"></i></a>
         </div>
 
         {/* Dock - paling bawah di mobile */}
