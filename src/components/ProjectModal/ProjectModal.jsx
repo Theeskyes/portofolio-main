63d94b0d629e62ce7af3d659d187a2ab32cb690f
Theeskyes <galaksiputra2009@gmail.com>
2026-06-09 08:16:05 +0700

    PERUBAHAN BESAR KARTU

--- src/components/ProjectModal/ProjectModal.jsx
+++ src/components/ProjectModal/ProjectModal.jsx
@@ -72,7 +72,7 @@ const ProjectModal = ({ isOpen, onClose, project }) => {
                 className="mt-4 inline-flex items-center justify-center gap-2 font-semibold bg-violet-600 p-3 px-5 rounded-full w-full cursor-pointer border border-transparent hover:bg-violet-700 transition-colors"
             >
                 <FiYoutube />
-                <span>Watch Video</span>
+                <span>Watch VideoA</span>
             </a>
         </div>
       </div>
